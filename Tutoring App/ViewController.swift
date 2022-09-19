7eef0f9efecfbe3280b740f935b90c45b2d3d278
bczerwiec <bczerwiec5088@stu.d214.org>
2022-09-19 10:22:30 -0500

    Changes

--- Tutoring App/ViewController.swift
+++ Tutoring App/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tutoringLabel: UILabel!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var firstNameLabel: UITextField!
+    @IBOutlet weak var lastNameLabel: UITextField!
+    @IBOutlet weak var idLabel: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    var fullName: String = ""
+    var IDNumber: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signIn(_ sender: UIButton) {
+        tutoringLabel.isHidden = true
+        signInButton.isHidden = true
+        firstNameLabel.isHidden = false
+        lastNameLabel.isHidden = false
+        idLabel.isHidden = false
+        enterButton.isHidden = false
+        
+    }
+    @IBAction func enter(_ sender: UIButton) {
+        if firstNameLabel.text == "" {
+            let dialogMessage = UIAlertController(title: "Error", message: "Box Left Empty", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+             })
+            dialogMessage.addAction(ok)
+            self.present(dialogMessage, animated: true, completion: nil)
+        }
+        else {
+            if lastNameLabel.text == "" {
+                let dialogMessage = UIAlertController(title: "Error", message: "Box Left Empty", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                 })
+                dialogMessage.addAction(ok)
+                self.present(dialogMessage, animated: true, completion: nil)
+            }
+            else {
+                if idLabel.text == "" {
+                    let dialogMessage = UIAlertController(title: "Error", message: "Box Left Empty", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                     })
+                    dialogMessage.addAction(ok)
+                    self.present(dialogMessage, animated: true, completion: nil)
+                }
+                else {
+                    fullName = "\(firstNameLabel.text!) \(lastNameLabel.text!)"
+                    IDNumber = idLabel.text!
+                    firstNameLabel.isHidden = true
+                    lastNameLabel.isHidden = true
+                    idLabel.isHidden = true
+                    enterButton.isHidden = true
+                    tutoringLabel.isHidden = false
+                }
+            }
+        }
+ 
+        print(fullName)
+        print(IDNumber)
+        print(firstNameLabel.isHidden, lastNameLabel.isHidden, idLabel.isHidden)
+    }
+    
+    
 }
 
